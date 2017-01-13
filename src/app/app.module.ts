cd711d15e1c1f46935d967c1330d6ddf60dda719
Johan Alvarez <llstarscreamll@hotmail.com>
2017-01-13 13:34:07 -0500

    probando shells

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
+// ng2-bootstrap
+import { DropdownModule } from 'ng2-bootstrap/dropdown';
+
 import { AppComponent } from './app.component';
+import { AdminLTENavTopComponent } from './shells/adminLTE/admin-lte-nav-top/admin-lte-nav-top.component';
+import { AdminLTESidebarComponent } from './shells/adminLTE/admin-lte-sidebar/admin-lte-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLTENavTopComponent,
+    AdminLTESidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    DropdownModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
