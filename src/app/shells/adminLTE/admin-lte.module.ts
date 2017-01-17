b4671eb60ef04b4c880ada0e07d5f2735d4033d5
Johan Alvarez <llstarscreamll@hotmail.com>
2017-01-16 22:59:47 -0500

    añadiendo links a otras rutas de la app

--- src/app/shells/adminLTE/admin-lte.module.ts
+++ src/app/shells/adminLTE/admin-lte.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // ng2-bootstrap
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
 
 import { AdminLteControlSidebarComponent } from './_partials/admin-lte-control-sidebar/admin-lte-control-sidebar.component';
-import { SidebarLayoutHeaderComponent } from './_partials/sidebar-layout-header/sidebar-layout-header.component';
 import { AdminLteFooterComponent } from './_partials/admin-lte-footer/admin-lte-footer.component';
 import { AdminLteMainSidebarComponent } from './_partials/admin-lte-main-sidebar/admin-lte-main-sidebar.component';
 import { AdminLteUserAccountMenuComponent } from './_partials/admin-lte-user-account-menu/admin-lte-user-account-menu.component';
@@ -18,12 +18,11 @@ export const COMPONENTS = [
   AdminLteNavTopLayoutComponent,
   AdminLteSidebarLayoutComponent,
   AdminLteControlSidebarComponent,
-  AdminLteUserAccountMenuComponent,
-  SidebarLayoutHeaderComponent
+  AdminLteUserAccountMenuComponent
 ];
 
 @NgModule({
-  imports: [CommonModule, DropdownModule.forRoot()],
+  imports: [CommonModule, RouterModule, DropdownModule.forRoot()],
   providers: [],
   declarations: COMPONENTS,
   exports: COMPONENTS,
