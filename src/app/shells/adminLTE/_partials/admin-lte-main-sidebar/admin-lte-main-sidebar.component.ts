0bacc16df379ef8d1ff6d24bf8e28b00e75f7c73
Johan Alvarez <llstarscreamll@hotmail.com>
2017-01-17 15:51:18 -0500

    formateando código

--- src/app/shells/adminLTE/_partials/admin-lte-main-sidebar/admin-lte-main-sidebar.component.ts
+++ src/app/shells/adminLTE/_partials/admin-lte-main-sidebar/admin-lte-main-sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from './../../../../core/models/MenuItem'; 
+import { MenuItem } from './../../../../core/models/MenuItem';
 
 @Component({
   selector: '[app-admin-lte-main-sidebar]',
@@ -9,19 +9,23 @@ import { MenuItem } from './../../../../core/models/MenuItem';
 export class AdminLteMainSidebarComponent implements OnInit {
 
   public menuItems: MenuItem[] = [
-    { label: 'Header', icon:'', url: '', visivility: 'private', isHeader: true, childs: [] },
-    { label: 'menu 1', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-    { label: 'public menu 2', icon:'fa fa-link', url: '/foo/foo', visivility: 'public', isHeader: false, childs: [] },
-    { label: 'tree 1', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [
-      { label: 'sub-menu 1', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-      { label: 'sub-menu 2', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-      { label: 'sub-menu 3', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-    ] },
-    { label: 'tree 2', icon:'fa fa-home', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [
-      { label: 'sub-menu 1', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-      { label: 'sub-menu 2', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-      { label: 'sub-menu 3', icon:'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
-    ] }
+    { label: 'Header', icon: '', url: '', visivility: 'private', isHeader: true, childs: [] },
+    { label: 'Home', icon: 'fa fa-home', url: '/front/landing', visivility: 'private', isHeader: false, childs: [] },
+    { label: 'public menu 2', icon: 'fa fa-link', url: '/foo/foo', visivility: 'public', isHeader: false, childs: [] },
+    {
+      label: 'tree 1', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [
+        { label: 'sub-menu 1', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+        { label: 'sub-menu 2', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+        { label: 'sub-menu 3', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+      ]
+    },
+    {
+      label: 'tree 2', icon: 'fa fa-home', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [
+        { label: 'sub-menu 1', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+        { label: 'sub-menu 2', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+        { label: 'sub-menu 3', icon: 'fa fa-link', url: '/foo/foo', visivility: 'private', isHeader: false, childs: [] },
+      ]
+    }
   ];
 
   public selectedItemIndex: number = 0;
@@ -30,7 +34,7 @@ export class AdminLteMainSidebarComponent implements OnInit {
 
   public ngOnInit() { }
 
-  public selectedMenuItem(itemIndex: number){
+  public selectedMenuItem(itemIndex: number) {
     this.selectedItemIndex = itemIndex;
   }
 
