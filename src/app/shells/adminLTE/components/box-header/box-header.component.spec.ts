5d258f38c7ac7fd4f091452d890a7d1d71d99081
Johan Alvarez <llstarscreamll@hotmail.com>
2017-01-18 02:32:36 -0500

    corrigiendo path y nombre de componente

--- src/app/shells/adminLTE/components/box-header/box-header.component.spec.ts
+++ src/app/shells/adminLTE/components/box-header/box-header.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { BoxTitleComponent } from './box-title.component';
+import { BoxHeaderComponent } from './box-header.component';
 
-describe('BoxTitleComponent', () => {
-  let component: BoxTitleComponent;
-  let fixture: ComponentFixture<BoxTitleComponent>;
+describe('BoxHeaderComponent', () => {
+  let component: BoxHeaderComponent;
+  let fixture: ComponentFixture<BoxHeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoxTitleComponent ]
+      declarations: [ BoxHeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BoxTitleComponent);
+    fixture = TestBed.createComponent(BoxHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
