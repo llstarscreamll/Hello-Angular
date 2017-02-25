5f732ca5ed31e788f46218be32c3ffeb0bdeefa2
Johan Alvarez <llstarscreamll@hotmail.com>
2017-02-24 21:53:02 -0500

    better style for paginator

--- src/app/modules/core/components/pagination.component.ts
+++ src/app/modules/core/components/pagination.component.ts
@@ -4,12 +4,17 @@ import { Pagination } from './../models/pagination';
 @Component({
   selector: 'app-pagination',
   template: `
-    <nav class="text-right">
-      <ul class="pagination pull right">
-        <li class="" [ngClass]="{ 'disabled': !pagination?.links?.previous }"><a href="#">Previous</a></li>
-        <li class="" [ngClass]="{ 'disabled': !pagination?.links?.next }"><a href="#">Next</a></li>
+    <nav>
+      <ul class="pagination pagination-lg pull-right">
+        <li class="" [ngClass]="{ 'disabled': !pagination?.links?.previous }">
+          <span role="button"><span aria-hidden="true">&laquo;</span></span>
+        </li>
+        <li class="" [ngClass]="{ 'disabled': !pagination?.links?.next }">
+          <span role="button"><span aria-hidden="true">&raquo;</span></span>
+        </li>
       </ul>
     </nav>
+    <div class="clearfix"></div>
     <div class="text-right">
       Mostrando {{ pagination?.count }} de {{ pagination?.total }} registros
     </div>
