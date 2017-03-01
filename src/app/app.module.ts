2c60bb9a503ac1da8c915a319b79285bf913be65
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-01 03:22:28 -0500

    add on working models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { StoreModule } from '@ngrx/store';
-import { RouterStoreModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-
 // ng2 Translate
 import {TranslateModule, TranslateService} from 'ng2-translate';
 
 // Shell
 import { AdminLTEShellModule } from './shells/adminLTE/admin-lte.module';
-
+import { AppComponent } from './app.component';
+import { AppEffects } from './modules/core/effects/app';
+import { AppRoutingModule } from './app-routing.module';
+import { AppService } from './modules/core/services/app';
+import { AuthModule } from './modules/auth/auth.module';
+import { BookModule } from './modules/book/book.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CompanyModule } from './modules/company/company.module';
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule } from '@angular/forms';
 // Modules
 import { FrontModule } from './modules/front/front.module';
-import { WelcomeModule } from './modules/welcome/welcome.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { PayrollModule } from './modules/payroll/payroll.module';
-import { CompanyModule } from './modules/company/company.module';
-
+import { GeneratorModule } from './modules/generator/generator.module';
+import { HttpModule } from '@angular/http';
 // Separated files for core module, TODO: wrap everything from core module on NgModule Class
 import { LocalStorageService } from './modules/core/services/localStorage';
-import { AppService } from './modules/core/services/app';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { NgModule } from '@angular/core';
+import { PayrollModule } from './modules/payroll/payroll.module';
+import { RouterStoreModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { WelcomeModule } from './modules/welcome/welcome.module';
 import { reducer } from './modules/core/reducers';
-import { AppEffects } from './modules/core/effects/app';
 
 @NgModule({
   declarations: [
@@ -50,7 +46,9 @@ import { AppEffects } from './modules/core/effects/app';
     WelcomeModule,
     AuthModule,
     PayrollModule,
-    CompanyModule
+    CompanyModule,
+    GeneratorModule,
+    BookModule
   ],
   providers: [
     LocalStorageService, AppService
