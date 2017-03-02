53d69ff8f86c6600237cea5c53cdcbd7994b0b20
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-02 12:41:02 -0500

    clean basicForm component view

--- src/app/modules/core/components/basicSearch.component.ts
+++ src/app/modules/core/components/basicSearch.component.ts
@@ -3,31 +3,17 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-basic-search',
   template: `
-        <form name="basicSearch" (ngSubmit)="search.emit({search: searchText})">
-          <div class="input-group">
-            <div class="input-group-btn">
-              <div class="btn-group" dropdown>
-                <button type="button" class="btn btn-default" dropdownToggle>
-                  <span class="caret"></span>
-                </button>
-                <ul dropdownMenu role="menu" aria-labelledby="single-button" class="dropdown-menu dropdown-menu-right">
-                  <li role="menuitem"><a class="dropdown-item" href="#">Action</a></li>
-                  <li role="menuitem"><a class="dropdown-item" href="#">Another action</a></li>
-                  <li role="menuitem"><a class="dropdown-item" href="#">Something else here</a></li>
-                  <li class="divider dropdown-divider"></li>
-                  <li role="menuitem"><a class="dropdown-item" href="#">Separated link</a></li>
-                </ul>
-              </div>
-            </div>
-            <input [(ngModel)]="searchText" name="searchText" class="form-control" placeholder="Buscar..." aria-label="Text input with segmented button dropdown">
-            <div class="input-group-btn">
-              <button type="submit" class="btn btn-default">
-                <span class="glyphicon glyphicon-search"></span>
-              </button>
-            </div>
-          </div>
-        </form>
-    `,
+  <form name="basicSearch" (ngSubmit)="search.emit({search: searchText})">
+    <div class="input-group">
+      <input [(ngModel)]="searchText" name="searchText" class="form-control" placeholder="Buscar..." aria-label="Text input with segmented button dropdown">
+      <div class="input-group-btn">
+        <button type="submit" class="btn btn-default">
+          <span class="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    </div>
+  </form>
+  `,
   styles: [':host { display: block; }']
 })
 export class BasicSearchComponent implements OnInit {
