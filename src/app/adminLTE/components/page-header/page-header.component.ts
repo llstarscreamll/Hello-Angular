fdf1cafd0e228cda6e1e0b8b0854005ee4817a88
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-16 23:59:07 -0500

    fix error on page header component

--- src/app/adminLTE/components/page-header/page-header.component.ts
+++ src/app/adminLTE/components/page-header/page-header.component.ts
@@ -3,11 +3,12 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-header',
   template: `<ng-content></ng-content>`,
-  styles: [':host { display: block; }']
+  styles: [':host { display: inline-block; }']
 })
 export class PageHeaderComponent implements OnInit {
 
   @HostBinding('class.content-header') headerClass: boolean = true;
+  @HostBinding('class.row') rowClass: boolean = true;
 
   constructor() { }
 
