ffe6003ea0099333a601d9d91e7c0211b38d7ec2
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-28 20:24:35 -0500

    update to new apiato domain

--- src/environments/env.example.ts
+++ src/environments/env.example.ts
@@ -4,8 +4,8 @@ import { AdminLTEShellModule } from './../app/adminLTE/admin-lte.module';
  * This files has environment variables for the app. 
  */
 export const ENV = {
-    // put your API url here, like 'http://api.hello.dev/api'
-    API_URL: 'http://api.hello.dev/api',
+    // put your API url here, like 'http://api.apiato.dev/api'
+    API_URL: 'http://api.apiato.dev/api',
     // the application theme, it should be defined
     THEME: AdminLTEShellModule,
 };
