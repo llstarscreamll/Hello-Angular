56f11fd35a4e446626d0d759f6c339e984599b42
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-13 02:26:30 -0500

    update core module

--- src/app/core/models/MenuItem.ts
+++ src/app/core/models/MenuItem.ts
@@ -2,7 +2,8 @@ export class MenuItem {
   label: string;
   url: string;
   icon: string;
-  childs: MenuItem[];
+  childs?: MenuItem[];
   isHeader: boolean;
   visivility: string;
+  permissions: string[]; // permissions that says if the menu item should be displayed
 }
--- src/app/core/services/abstract.service.ts
+++ src/app/core/services/abstract.service.ts
@@ -7,12 +7,11 @@ import * as _ from 'lodash';
 
 import { environment } from './../../../environments/environment';
 import * as fromRoot from './../../reducers';
-import * as appMsgActions from './../../core/actions/app-message.actions';
 
 /**
  * Abstract Class Service.
  */
-export abstract class Service {
+export abstract class AbstractService {
 
   protected headers: Headers;
   protected domain: string = environment.api_url;
@@ -26,13 +25,14 @@ export abstract class Service {
   }
 
   protected setAuthorizationHeader() {
-    this.headers.set('authorization', sessionStorage.getItem('token_type') +' ' + sessionStorage.getItem('token'));
+    let authToken = localStorage.getItem('token_type') + ' ' + localStorage.getItem('token');
+    this.headers.set('authorization', authToken);
   }
 
   /**
    * Returns full API endpoint.
    */
-  protected apiEndpoint(path: string = ''): string {
+  protected apiEndpoint(path: string | number = ''): string {
     let url = this.domain;
     url += (this.API_ENDPOINT !== '') ? this.API_ENDPOINT + '/' : '';
 
@@ -59,60 +59,91 @@ export abstract class Service {
     return Observable.throw(body);
   }
 
-  protected parseGetParams(data: Object = {}) {
+  /**
+   * Parse a query object to url params, which will be sent in http.get method.
+   * This method try to convert the given query object to url params that the
+   * Laravel PHP l5-repository package understands, for more info read the
+   * package docs:
+   * https://github.com/andersao/l5-repository#using-the-requestcriteria
+   */
+  protected parseGetParams(query: Object = {}): URLSearchParams {
     let urlParams: URLSearchParams = new URLSearchParams;
-    let tmpArray = new Array();
 
-    if (_.get(data, 'advanced_search', false)) {
-      _.forOwn(data, (value, key) => {
-        // the value it's an object?
+    // parse for advanced search cases
+    if (_.get(query, 'advanced_search', false)) {
+      _.forOwn(query, (value, key) => {
+        // parse object
         if (_.isPlainObject(value)) {
-          _.forOwn(value, (subValue) => { subValue != '' ? urlParams.append(key+'[]', subValue) : null; });
-          console.info();
+          _.forOwn(value, (subValue) => {
+            subValue != ''
+              ? urlParams.append(key + '[]', subValue)
+              : null;
+          });
+
+          // parse array
         } else if (_.isArray(value)) {
-          _.forOwn(value, (subValue => { urlParams.append(key+'[]', subValue) }));
+          _.forOwn(value, (subValue => {
+            urlParams.append(key + '[]', subValue);
+          }));
+
+          // parse flat key => value
         } else {
           urlParams.set(key, value);
         }
       });
-
-      return urlParams;
     }
 
-    _.forOwn(data, (value, key) => {
+    // parse other query options
+    _.forOwn(query, (value, key) => {
       // parse search, sortedBy and page param
-      if (key == "search" || key == "sortedBy" || key == 'page' || key == 'trashed') {
+      if (key == "search" || key == "sortedBy" || key == 'page' || key == 'trashed' || key == "orderBy") {
         urlParams.set(key, value);
       }
 
-      // parse orderBy param
-      if (key == "orderBy") {
-        if (_.has(data, 'orderByRelationsMap.'+value)) {
-          //console.warn(_.get(data, 'orderByRelationsMap.'+value) as String);
-          urlParams.set(key, _.get(data, 'orderByRelationsMap.'+value) as string);
-        } else {
-          urlParams.set(key, value);
-        }
-      }
-
-      // parse columns param
+      // parse filter param, whick means the columns or model attrs to retrieve.
+      // From this:
+      // { filter: ['id', 'name', 'author_id', 'published_at', 'user_id'] }
+      //
+      // to this:
+      // { filter: "id;name;author_id;published_at;user_id" }
       if (key == "filter") {
-        // concat the required_columns with the filter columns given on parameters
+        // concat the required_columns with the filter columns given query object
         urlParams.set('filter', _.join(this.required_columns.concat(value), ';'));
       }
 
-      // parse include model relations
+      // parse include model relations. The required attrs relations should
+      // given on a key(the filed needed to retrieve the relation) and value
+      // (the model relation name) object map:
+      // include: {
+      //   'book.author_id': 'author',
+      //   'book.user_id': 'createdBy',
+      // }
+      //
+      // so, if the query object has { filter: ['book.author_id', 'book.user_id'] },
+      // then the respective relation will be append to the url param, like this:
+      // { include: "author,createdBy" }
+      //
+      // and the required fields will be appened to the filter url param:
+      // { filter: "id;name;published_at;author_id;user_id" }
+      //
+      // NOTE: the word "model" is from the backend side, e.g. the Book model, which
+      // has relations with User and Author models:
+      // Book::first()->author;
+      // Book::first()->createdBy;
       if (key == "include") {
-        tmpArray = [];
+        let relations: string[] = []; // here we collect the model relations to include
+        let attrsToFilter = _.get(query, 'filter', []); // the inclues
+
         // iterate over the include object
-        _.forOwn(value, (includeValue, includeKey) => {
+        _.forOwn(value, (relationName, requiredAttr) => {
           // if the include key exist on the columns array, then collect the include value
-          if (_.includes(_.get(data, 'filter', []), includeKey)) {
-            tmpArray.push(includeValue);
+          if (_.includes(attrsToFilter, requiredAttr)) {
+            relations.push(relationName);
           }
         });
-        // join the array with "," and set the url param
-        urlParams.set(key, _.join(tmpArray, ','));
+
+        // join the array with "," and set the "include" url param
+        urlParams.set(key, _.join(relations, ','));
       }
     });
 
