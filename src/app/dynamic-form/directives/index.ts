50b3c8d69261643a4736e7934dc47fb5386120db
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-13 02:20:43 -0500

    remove dynamic-form feature module

--- src/app/dynamic-form/directives/index.ts
+++
@@ -1,9 +0,0 @@
-import { DynamicControlDirective } from './dynamic-control.directive';
-import { DynamicGroupDirective } from './dynamic-group.directive';
-import { DatetimeDirective } from './datetime.directive';
-
-export const DIRECTIVES = [
-    DynamicControlDirective,
-    DynamicGroupDirective,
-    DatetimeDirective
-];
