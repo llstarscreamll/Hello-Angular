18917d0d2dae26f81923c0e8e3d099e7314bf55c
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-15 23:53:05 -0500

    add some translations strings

--- src/app/core/translations/es.ts
+++ src/app/core/translations/es.ts
@@ -2,11 +2,15 @@ export const ES = {
     'SHELL': {
         'Login': 'Iniciar Sesión',
         'Log out': 'Cerrar Sesión',
-        'Sign in': 'Crear cuenta',
+        'Sign up': 'Crear cuenta',
+        'My Account': 'Mi Cuenta',
+        'Member since': 'Miembro desde',
         'Search': 'Buscar',
         'alert-danger': 'Error',
         'alert-warning': 'Alerta',
         'alert-success': 'Bien Hecho',
         'Welcome to': 'Bienvenido a',
+        'Hi': 'Hola',
+        'Dashboard': 'Dashboard',
     }
 };
